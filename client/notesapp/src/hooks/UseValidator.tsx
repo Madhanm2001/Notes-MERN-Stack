c290188c11b23994e1d13d69daf1662f34cdc6fe
Madhan <mmadhankumar519@gmail.com>
2025-06-26 13:19:49 +0530

    changes in useValidator logic

--- client/notesapp/src/hooks/UseValidator.tsx
+++ client/notesapp/src/hooks/UseValidator.tsx
@@ -1,60 +1,60 @@
-// import pattern from '../constants/RegexPattern'
-
-import { useState } from "react"
-import pattern from "../constants/RegexPattern"
+import { useState } from "react";
+import pattern from "../constants/RegexPattern";
 
 const UseValidator = () => {
+  const [signInError, setSignInError] = useState<any>({});
+  const [signUpError, setSignUpError] = useState<any>({});
 
-    const signUpValidator = (state: any) => {
+  const signInValidator = (state: any) => {
+    const error: any = {};
 
-        const [signUpError, setSignUpError] = useState({})
-
-        const error: any = {}
-
-        if (!state.username) {
-            error.username = "Username should not be empty";
-        } else if (!pattern.USER_NAME.test(state.username)) {
-            error.username = "Invalid username format";
-        }
-
-        if (!state.password) {
-            error.password = "Password should not be empty";
-        } else if (!pattern.PASSWORD.test(state.password)) {
-            error.password = "Password should be at least 6 characters";
-        }
-
-        setSignUpError(error)
-
-        return {error, signUpError}
-
+    if (!state.username) {
+      error.username = "Username should not be empty";
+    } else if (!pattern.USER_NAME.test(state.username)) {
+      error.username = "Invalid username format";
     }
 
-    const signInValidator = (state: any) => {
-
-        const [signInError, setSignInError] = useState({})
-
-        const error: any = {}
-
-        if (!state.username) {
-            error.username = "Username should not be empty";
-        } else if (!pattern.USER_NAME.test(state.username)) {
-            error.username = "Invalid username format";
-        }
-
-        if (!state.password) {
-            error.password = "Password should not be empty";
-        } else if (!pattern.PASSWORD.test(state.password)) {
-            error.password = "Password should be at least 6 characters";
-        }
-
-        setSignInError(error)
-
-        return {error, signInError}
-
+    if (!state.password) {
+      error.password = "Password should not be empty";
+    } else if (!pattern.PASSWORD.test(state.password)) {
+      error.password = "Password should be at least 6 characters";
     }
 
-    return {signUpValidator,signInValidator}
+    setSignInError(error);
+    return error
+  };
 
-}
+  const signUpValidator = (state: any) => {
+    const error: any = {};
 
-export default UseValidator
+    if (!state.username) {
+      error.username = "Username should not be empty";
+    } else if (!pattern.USER_NAME.test(state.username)) {
+      error.username = "Invalid username format";
+    }
+
+    if (!state.email) {
+      error.email = "Email should not be empty";
+    } else if (!pattern.EMAIL.test(state.email)) {
+      error.email = "Invalid email format";
+    }
+
+    if (!state.password) {
+      error.password = "Password should not be empty";
+    } else if (!pattern.PASSWORD.test(state.password)) {
+      error.password = "Password should be at least 6 characters";
+    }
+
+    setSignUpError(error);
+    return error
+  };
+
+  return {
+    signInValidator,
+    signUpValidator,
+    signInError,
+    signUpError,
+  };
+};
+
+export default UseValidator;
