10b0bc64f7c235c0f959cd6c416f87a551e6a265
Madhan <mmadhankumar519@gmail.com>
2025-06-28 15:24:27 +0530

    change the style of filter and sort option change navbar z-index

--- client/notesapp/src/component/AllNotes.tsx
+++ client/notesapp/src/component/AllNotes.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import AddNotes from '../images/AddNotes.jpg'
 import deleteLogo from '../images/delete.png'
 import editLogo from '../images/edit.jpg'
@@ -10,26 +10,39 @@ import filterIcon from '../images/filterIcon.jpg'
 import '../styles/folder.css'
 import searchLogo from '../images/searchLogo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowDownShortWide, faArrowUpAZ, faArrowUpShortWide, faArrowUpZA } from '@fortawesome/free-solid-svg-icons';
-
+import { faArrowDownShortWide, faArrowUpAZ, faArrowUpShortWide,faBars, faFilter, faArrowUpZA, faList, faListSquares } from '@fortawesome/free-solid-svg-icons';
+import pinnedLogo from '../images/ispinned.jpg' 
 
 
 const AllNotes: React.FC = () => {
 
-  const folderList = [ "workout routine", "workout routine", "workout routine", "workout routine", "workout routine", "workout routine"]
+  const folderList = [{data:"workout routine",isPinned:false}, {data:"workout routine",isPinned:true}, {data:"workout routine",isPinned:false}, {data:"workout routine",isPinned:false}, {data:"workout routine",isPinned:true}, {data:"workout routine",isPinned:false}]
   const sortOptions = ['A-Z', 'Z-A', 'old-new', 'new-old']
+  const [filterVal, setFilterVal] = useState('All')
+  const [isFilter, setIsFilter] = useState(false)
+  const [isSort, setIsSort] = useState(false)
+  const [SortVal, setSortVal] = useState('')
+
+  const onClickFilter = (data: string | undefined) => {
+    setFilterVal(data ? data : '')
+  }
+  const onClickPinned=(id:any)=>{
+
+ 
+
+  }
   return (
     <div>
-      <div className='flex flex-wrap z-[-100] justify-between m-[25px] relative'>
+      <div className='flex flex-wrap justify-between m-[25px] relative'>
         <img src={AddNotes} id="Addnotes" title='Add-Notes' className='cursor-pointer' alt="" />
         <div className='flex gap-[80px] max-sm:gap-[10px] flex-wrap relative'>
-          <span className='flex'>
+          <span className='flex gap-1'>
             <input
               type="text"
-              className='border-[2px] border-white h-[35px] sm:w-[500px] w-[200px] rounded-tl rounded-bl mt-[25px] max-sm:ml-[20px]'
+              className='border-[2px] border-white text-white h-[35px] sm:w-[500px] w-[200px] rounded mt-[25px] max-sm:ml-[20px]'
             />
             <img
-              className='h-[10px] w-[10px] mt-[25px] h-[35px] w-[35px] p-[8px] bg-[#313131] rounded-tr rounded-br'
+              className='h-[10px] w-[10px] mt-[25px] p-[0_10px] h-[35px] w-[35px] p-[8px] bg-[#313131] rounded'
               src={searchLogo}
               alt=""
             />
@@ -38,7 +51,8 @@ const AllNotes: React.FC = () => {
         <p className='text-white mt-[25px] max-sm:ml-[20px]'>clear all</p>
       </div>
 
-      <div className='flex flex-wrap justify-around max-[430px]:flex-col max-[430px]:items-center'>
+      {/*old filter and sort tags
+       <div className='flex flex-wrap justify-around max-[430px]:flex-col max-[430px]:items-center'>
         <ul className='flex justify-center gap-[10%]'>
           <li><FontAwesomeIcon icon={faArrowUpAZ} style={{ width: "20px", height: "20px" }} className='fa-sharp fa-solid fa-arrow-up-a-z bg-[#cdca00] p-2 rounded' /></li>
           <li><FontAwesomeIcon icon={faArrowUpZA} style={{ width: "20px", height: "20px" }} className="fa-solid fa-arrow-up-z-a bg-[#cdca00] p-2 rounded" /></li>
@@ -56,24 +70,56 @@ const AllNotes: React.FC = () => {
         </select>
 
 
+      </div> */}
+
+      <div className='flex justify-between'>
+        <li onClick={() => setIsFilter(!isFilter)}><FontAwesomeIcon icon={faFilter} style={{ width: "25px", height: "25px" }} className={`fa-solid fa-bars-filter ${isFilter ? 'text-black bg-[#cdca00]' : 'text-white bg-[#313131]'} p-2 rounded`} /></li>
+        <li><FontAwesomeIcon onClick={() => setIsSort(!isSort)} icon={faListSquares} style={{ width: "25px", height: "25px" }} className={`fa-solid fa-bars-filter ${isSort ? 'text-black bg-[#cdca00]' : 'text-white bg-[#313131]'} p-2 rounded`} /></li>
       </div>
+
+      <div className='flex justify-between w-[95%]'>
+        <div className='flex bg-black p-3 gap-[3%] rounded mt-[5px] max-[480px]:w-[40%] w-[50%] flex-wrap'>
+          
+            {isFilter &&<>
+              <button onClick={() => onClickFilter('All')} className={`p-[2px_10px] w-[100px] h-[30px] mt-[5px] ${filterVal === 'All' ? 'text-black bg-[#cdca00]' : 'text-white bg-[#5c5c5c]'} font-semibold rounded-[20px]`}>All</button>
+              <button onClick={() => onClickFilter('Active')} className={`p-[2px_10px] w-[100px] h-[30px] mt-[5px] ${filterVal === 'Active' ? 'text-black bg-[#cdca00]' : 'text-white bg-[#5c5c5c]'} font-semibold rounded-[20px]`}>Active</button>
+              <button onClick={() => onClickFilter('Archived')} className={`p-[2px_10px] w-[100px] h-[30px] mt-[5px] ${filterVal === 'Archived' ? 'text-black bg-[#cdca00]' : 'text-white bg-[#5c5c5c]'} font-semibold rounded-[20px]`}>Archived</button>
+              </> }
+        </div>
+
+        {isSort && <ul className='flex max-[400px]:gap-[3%] gap-[10%] mt-[20px] max-[500px]:flex-col'>
+          <li><FontAwesomeIcon icon={faArrowUpAZ} style={{ width: "20px", height: "20px" }} onClick={() => setSortVal('a-z')} className={`fa-sharp fa-solid fa-arrow-up-a-z ${SortVal === 'a-z' ? 'text-black bg-[#cdca00]' : 'text-white bg-[#5c5c5c]'} p-2 rounded`} /></li>
+          <li><FontAwesomeIcon icon={faArrowUpZA} style={{ width: "20px", height: "20px" }} onClick={() => setSortVal('z-a')} className={`fa-sharp fa-solid fa-arrow-up-a-z ${SortVal === 'z-a' ? 'text-black bg-[#cdca00]' : 'text-white bg-[#5c5c5c]'} p-2 rounded`} /></li>
+          <li><FontAwesomeIcon icon={faArrowDownShortWide} style={{ width: "20px", height: "20px" }} onClick={() => setSortVal('new to old')} className={`fa-sharp fa-solid fa-arrow-up-a-z ${SortVal === 'new to old' ? 'text-black bg-[#cdca00]' : 'text-white bg-[#5c5c5c]'} p-2 rounded`} /></li>
+          <li><FontAwesomeIcon icon={faArrowUpShortWide} style={{ width: "20px", height: "20px" }} onClick={() => setSortVal('old to new')} className={`fa-sharp fa-solid fa-arrow-up-a-z ${SortVal === 'old to new' ? 'text-black bg-[#cdca00]' : 'text-white bg-[#5c5c5c]'} p-2 rounded`} /></li>
+        </ul>}
+
+
+      </div>
+
+
+
+
+      {/* <div className='flex gap-[3%]'>
+        <button className='bg-[#cdca00] p-[2px_10px] text-black font-semibold rounded-[20px]'>All</button>
+        <button className='bg-[#cdca00] p-[2px_10px] text-black font-semibold rounded-[20px]'>Active</button>
+        <button className='bg-[#cdca00] p-[2px_10px] text-black font-semibold rounded-[20px]'>Archived</button>
+      </div> */}
 
 
       {/* <h3 className='text-center text-white font-sans font-weight-[4vh] m-[2.5vh] text-[4vh]'>Folders</h3> */}
 
       <ul id='folders'>
         {folderList.map((data, id) => {
-
           return <li key={id} className='shadow-[0px_0px_10px_2px_#989898] p-[15px] rounded-[10px] my-[10px] '>
-            <img src={notpinnedLogo} alt="" className='h-[30px] w-[30px] my-[10px] ms-auto' />
-            <img className='mx-auto my-[8px]' id='noteslogo' src={appLogo} alt="" /><p id='folderName'>{data}</p>
+            <img src={data.isPinned?pinnedLogo:notpinnedLogo} onClick={onClickPinned} alt="" className='h-[30px] w-[30px] my-[10px] ms-auto' />
+            <img className='mx-auto my-[8px]' id='noteslogo' src={appLogo} alt="" /><p id='folderName' className='text-center truncate w-full'>{data.data}</p>
             <div className='flex justify-center gap-[20px] my-[10px]'>
               <img src={deleteLogo} alt="" className='h-[25px] w-[25px] my-[10px]' />
               <img src={editLogo} alt="" className='h-[25px] w-[25px] my-[10px]' />
               <img src={ArchivedLogo} alt="" className='h-[25px] w-[25px] my-[10px]' />
             </div>
           </li>
-
         })}
       </ul>
     </div>
--- client/notesapp/src/component/folder.tsx
+++ client/notesapp/src/component/folder.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AddFolder from '../images/AddFolder.png'
 import folderLogo from '../images/folderLogo.jpg'
 import deleteLogo from '../images/delete.png'
@@ -8,24 +8,34 @@ import editLogo from '../images/edit.jpg'
 import '../styles/folder.css'
 import searchLogo from '../images/searchLogo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowDownShortWide, faArrowUpAZ, faArrowUpShortWide, faArrowUpZA } from '@fortawesome/free-solid-svg-icons';
+import { faArrowDownShortWide, faArrowUpAZ, faArrowUpShortWide,faFilter,faArrowUpZA ,faBars,faListSquares} from '@fortawesome/free-solid-svg-icons';
 
 const Folder: React.FC = () => {
 
-  const folderList = ["workout routine", "workout routine", "workout routine", "workout routine", "workout routine", "workout routine"]
+  const folderList = ["workout routine","workout routineworkout routineworkout routineworkout routineworkout routineworkout routineworkout routineworkout routineworkout routine climbing",'chess']
+   const FilterList = ["workout routine","mountain climbing",'chess']
   const sortOptions = ['A-Z', 'Z-A', 'old-new', 'new-old']
+  const[filterVal,setFilterVal]=useState('')
+   const[isFilter,setIsFilter]=useState(false)
+   const[isSort,setIsSort]=useState(false)
+   const[SortVal,setSortVal]=useState('')
+  
+    const onClickFilter=(e:string|any)=>{
+      setFilterVal(e?e.target.value:'')
+    }
+    
   return (
     <div>
-    <div className='flex flex-wrap z-[-100] justify-between m-[25px] relative'>
+    <div className='flex flex-wrap justify-between m-[25px] relative'>
         <img src={AddFolder} id="Addfolder" title='Add-Folder' className='cursor-pointer' alt="" />
         <div className='flex gap-[80px] max-sm:gap-[10px] flex-wrap relative'>
-          <span className='flex'>
+          <span className='flex gap-1'>
             <input
               type="text"
-              className='border-[2px] border-white h-[35px] sm:w-[500px] w-[200px] rounded-tl rounded-bl mt-[25px] max-sm:ml-[20px]'
+              className='border-[2px] border-white p-[0_10px] text-white h-[35px] sm:w-[500px] w-[200px] rounded mt-[25px] max-sm:ml-[20px]'
             />
             <img
-              className='h-[10px] w-[10px] mt-[25px] h-[35px] w-[35px] p-[8px] bg-[#313131] rounded-tr rounded-br'
+              className='h-[10px] w-[10px] mt-[25px] h-[35px] w-[35px] p-[8px] bg-[#313131] rounded'
               src={searchLogo}
               alt=""
             />
@@ -34,23 +44,24 @@ const Folder: React.FC = () => {
         <p className='text-white mt-[25px] max-sm:ml-[20px]'>clear all</p>
       </div>
 
-<div className='flex flex-wrap justify-around max-[430px]:flex-col max-[430px]:items-center'>
-        <ul className='flex gap-[100%]'>
-          <li><FontAwesomeIcon icon={faArrowUpAZ} style={{ width: "20px", height: "20px" }} className='fa-sharp fa-solid fa-arrow-up-a-z bg-[#cdca00] p-2 rounded' /></li>
-          <li><FontAwesomeIcon icon={faArrowUpZA} style={{ width: "20px", height: "20px" }} className="fa-solid fa-arrow-up-z-a bg-[#cdca00] p-2 rounded" /></li>
-          <li><FontAwesomeIcon icon={faArrowDownShortWide} style={{ width: "20px", height: "20px" }} className="fa-solid fa-arrow-down-wide-short bg-[#cdca00] p-2 rounded" /></li>
-          <li><FontAwesomeIcon icon={faArrowUpShortWide} style={{ width: "20px", height: "20px" }} className="fa-solid fa-arrow-down-wide-short bg-[#cdca00] p-2 rounded" /></li>
-        </ul>
-        <select
-          name=""
-          id=""
-          className="border-2 border-white rounded text-white bg-black px-3 py-2 w-[200px] max-[430px]:mt-[10%]"
-        >
-          <option value="all">All</option>
-          <option value="active">Fitness</option>
-          <option value="archived">MERN Stack Project-ideas</option>
-        </select>
+<div className='flex justify-between'>
+          <li onClick={()=>setIsFilter(!isFilter)}><FontAwesomeIcon icon={faFilter} style={{ width: "25px", height: "25px" }} className={`fa-solid fa-bars-filter ${isFilter?'text-black bg-[#cdca00]':'text-white bg-[#313131]'} p-2 rounded`} /></li>
+          <li><FontAwesomeIcon onClick={()=>setIsSort(!isSort)} icon={faListSquares} style={{ width: "25px", height: "25px" }} className={`fa-solid fa-bars-filter ${isSort?'text-black bg-[#cdca00]':'text-white bg-[#313131]'} p-2 rounded`} /></li>
+      </div>
 
+      <div className='flex justify-between w-[95%]'>
+        <div className='flex bg-black p-3 gap-[3%] rounded mt-[5px] w-[50%] flex-wrap'>
+{FilterList.map((data,id)=>{
+return  (isFilter&&<button value={data} onClick={(e)=>onClickFilter(e)} className={`p-[2px_10px] mt-[5px] ${filterVal===data?'text-black bg-[#cdca00]':'text-white bg-[#5c5c5c]'} font-semibold rounded-[20px]`}>{data}</button>)
+})}     
+ </div>
+
+        {isSort&&<ul className='flex max-[400px]:gap-[3%] gap-[10%] mt-[20px] max-[480px]:flex-col'>
+          <li><FontAwesomeIcon icon={faArrowUpAZ} style={{ width: "20px", height: "20px" }} onClick={()=>setSortVal('a-z')} className={`fa-sharp fa-solid fa-arrow-up-a-z ${SortVal==='a-z'?'text-black bg-[#cdca00]':'text-white bg-[#5c5c5c]'} p-2 rounded`} /></li>
+          <li><FontAwesomeIcon icon={faArrowUpZA} style={{ width: "20px", height: "20px" }} onClick={()=>setSortVal('z-a')} className={`fa-sharp fa-solid fa-arrow-up-a-z ${SortVal==='z-a'?'text-black bg-[#cdca00]':'text-white bg-[#5c5c5c]'} p-2 rounded`} /></li>
+          <li><FontAwesomeIcon icon={faArrowDownShortWide} style={{ width: "20px", height: "20px" }} onClick={()=>setSortVal('new to old')} className={`fa-sharp fa-solid fa-arrow-up-a-z ${SortVal==='new to old'?'text-black bg-[#cdca00]':'text-white bg-[#5c5c5c]'} p-2 rounded`} /></li>
+          <li><FontAwesomeIcon icon={faArrowUpShortWide} style={{ width: "20px", height: "20px" }} onClick={()=>setSortVal('old to new')} className={`fa-sharp fa-solid fa-arrow-up-a-z ${SortVal==='old to new'?'text-black bg-[#cdca00]':'text-white bg-[#5c5c5c]'} p-2 rounded`} /></li>
+        </ul>}
 
       </div>
       {/* <h3 className='text-center text-white font-sans font-weight-[4vh] m-[2.5vh] text-[4vh]'>Folders</h3> */}
@@ -58,10 +69,12 @@ const Folder: React.FC = () => {
       <ul id='folders'>
         {folderList.map((data, id) => {
 
-          return <li key={id} className='shadow-[0px_0px_10px_2px_#989898] p-[15px] rounded-[10px] my-[10px]'>
-            <img className='mx-auto my-[8px]' id='folderlogo' src={folderLogo} alt="" /><p id='folderName'>{data}</p>
-            <div className='flex justify-center gap-[20px] my-[10px]'>
-              <img src={deleteLogo} alt="" className='h-[25px] w-[25px] my-[10px]'/>
+          return <li key={id} className='shadow-[0px_0px_10px_2px_#989898] p-[15px] rounded-[10px] my-[10px] w-[150px]'>
+            <img className='mx-auto my-[8px]' id='folderlogo' src={folderLogo} alt="" /><p id='folderName' className='text-center truncate w-full'>
+  {data}
+</p>
+            <div className='flex justify-center gap-[20px] my-[10px] flex-wrap'>
+              <img src={deleteLogo} alt="" className='h-[25px] w-[25px] my-[10px] break words'/>
               <img src={editLogo} alt="" className='h-[25px] w-[25px] my-[10px]'/>
             </div>
           </li>
